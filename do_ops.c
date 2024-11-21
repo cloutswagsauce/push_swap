d1bdaf068343a3fbe82e011a26a2ae6f01fa0cfa
cloutswagsauce <105318787+cloutswagsauce@users.noreply.github.com>
2024-11-21 17:16:51 +0100

    Add files via upload

--- do_ops.c
+++ do_ops.c
@@ -1,14 +1,102 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   do_ops.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfaria-m <lfaria-m@42lausanne.ch>          +#+  +:+       +#+        */
+/*   By: lfaria-m <lfaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 11:36:07 by lfaria-m          #+#    #+#             */
-/*   Updated: 2024/11/20 11:36:48 by lfaria-m         ###   ########.fr       */
+/*   Updated: 2024/11/21 17:16:11 by lfaria-m         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "push_swap.h"
+
+void pa(t_list **stack_a, t_list **stack_b)
+{
+	t_list *temp;
+	
+	if (*stack_a && *stack_b)
+	{
+		temp = *stack_b;
+		*stack_b = (*stack_b)->next;
+		temp->next = *stack_a;
+		*stack_a = temp;
+	}
+	
+	
+	
+
+}
+
+void sa(t_list **stack_a)
+{
+	t_list *temp;
+	
+	if (*stack_a && (*stack_a)->next)
+	temp = (*stack_a)->next;
+	(*stack_a)->next = (*stack_a);
+	stack_a = temp;
+
+
+}
+
+void pb(t_list **stack_a, t_list **stack_b)
+{
+	
+}
+
+void sb(t_list **stack_b)
+{
+	t_list *temp;
+	if (*stack_b && (*stack_b)->next)
+	temp = (*stack_b)->next;
+	(*stack_b)->next = (*stack_b);
+	stack_b = temp;
+
+}
+void ss(t_list **stack_a, t_list **stack_b)
+{
+	t_list *temp;
+	
+	if (*stack_a && (*stack_a)->next)
+	temp = (*stack_a)->next;
+	(*stack_a)->next = (*stack_a);
+	stack_a = temp;
+	
+	if (*stack_b && (*stack_b)->next)
+	temp = (*stack_b)->next;
+	(*stack_b)->next = (*stack_b);
+	stack_b = temp;
+	
+}
+
+void ra(t_list **stack_a)
+{
+	
+}
+
+void rra(t_list **stack_a)
+{
+	
+}
+
+void rb(t_list **stack_b)
+{
+	
+}
+
+void rrb(t_list **stack_b)
+{
+	
+}
+void rr(t_list **stack_a, t_list **stack_b)
+{
+	
+}
+
+void rrr(t_list **stack_a, t_list **stack_b)
+{
+	
+}
 
--- main.c
+++ main.c
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfaria-m <lfaria-m@42lausanne.ch>          +#+  +:+       +#+        */
+/*   By: lfaria-m <lfaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 15:20:52 by lfaria-m          #+#    #+#             */
-/*   Updated: 2024/11/20 14:36:59 by lfaria-m         ###   ########.fr       */
+/*   Updated: 2024/11/21 16:29:29 by lfaria-m         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 # include "push_swap.h"
 
@@ -33,11 +33,7 @@ void add_to_stack(char **str, t_list **stack_a)
 	}
 	
 }
-void add_to_stack_int(int nbr, t_list **stack_a, int count)
-{
-	
-	
-}
+
 int  handle_input_str(char *str, t_list **stack_a)
 {
 	
@@ -50,37 +46,20 @@ int  handle_input_str(char *str, t_list **stack_a)
 	return (0);
 	
 }
-int  handle_input_int(int nbrs, int count, t_list **stack_a)
-{
-	int i = 0;
 
-	while (i < count)
-	{
-		add_to_stack(nbrs, stack_a);
-		i++;
-	}
-	
-
-	return (0);
-	
-}
 
 int main(int ac, char **av)
 {
 	t_list **stack_a;
 	//t_list **stack_b;
 	
-	int i = 1;
+	
 	if (ac == 1)
 		return 0 ;
 	stack_a = (t_list**)malloc(sizeof(t_list));
 	if (ac == 2)
 		handle_input_str(av[1], stack_a);
-	if (ac > 2)
-	{
-		while(i++ <= ac)
-			handle_input_int(av[i], ac, stack_a);
-	}
+	
 		
 	return (0);
 	
